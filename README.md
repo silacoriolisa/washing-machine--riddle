62ff15e6538530b391179f58d4183226af001a60
silacoriolisa <135693825+silacoriolisa@users.noreply.github.com>
2024-11-21 10:28:13 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Otwórz tę stronę na [https://silacoriolisa.github.io/washing-machine--riddle/](https://silacoriolisa.github.io/washing-machine--riddle/)
+
+## Użyj jako rozszerzenia
+
+To repozytorium można dodać jako **rozszerzenie** w MakeCode.
+
+* otwórz [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* kliknij na **Nowy Projekt**
+* kliknij **Rozszerzenia** w menu oznaczonym kołem zębatym
+* szukaj **https://github.com/silacoriolisa/washing-machine--riddle** i importuj
+
+## Edytuj ten projekt
+
+Aby edytować to repozytorium w MakeCode.
+
+* otwórz [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* kliknij **Importuj**, a następnie **Importuj URL**
+* wklej **https://github.com/silacoriolisa/washing-machine--riddle** i kliknij importuj
+
+#### Metadane (używane do wyszukiwania, renderowania)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,84 @@
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    basic.showIcon(IconNames.Umbrella)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Skull)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.TShirt)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (State == 1) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed - 100)
+    } else if (State == 2) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed - 100)
+    }
+})
+let Time = 0
+let Delay = 0
+let Speed = 0
+let State = 0
+basic.showArrow(ArrowNames.NorthEast)
+let myRiddle = MathRiddle.createMyRiddle()
+basic.showString("Ready")
+basic.forever(function () {
+    State = 0
+    if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+    basic.pause(200)
+    if (pins.digitalReadPin(DigitalPin.P12) == 0) {
+        if (Delay == 0) {
+            Delay = 10
+        } else if (Delay == 10) {
+            Delay = 15
+        } else if (Delay == 15) {
+            Delay = 0
+        }
+        basic.showString("" + (Delay))
+    }
+    Speed = 255
+    if (pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
+        myRiddle.puzzleBlock(MathRiddle.PuzzleBlockProperty.Y)
+        while (Delay > 0) {
+            basic.showString("" + (Delay))
+            basic.pause(1000)
+            Delay = Delay - 1
+        }
+        Time = 7
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
+        State = 1
+        while (Time > 0) {
+            basic.showString("" + (Time))
+            Time += -1
+            basic.pause(1000)
+            if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+                motor.motorStop(motor.Motors.M1)
+                Time = 0
+            }
+        }
+        motor.motorStop(motor.Motors.M1)
+        basic.pause(2000)
+        Time = 7
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
+        State = 2
+        while (Time > 0) {
+            basic.showString("" + (Time))
+            Time += -1
+            basic.pause(1000)
+            if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+                motor.motorStop(motor.Motors.M1)
+                Time = 0
+            }
+        }
+        motor.motorStop(motor.Motors.M1)
+        music.setVolume(255)
+        basic.pause(100)
+        basic.showString("END")
+    }
+})
